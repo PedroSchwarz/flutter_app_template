2ad964ba9171dc536d92000af52e60291038d5e9
Pedro Schwarz Rodrigues <pedroschawarz97@gmail.com>
2025-08-21 14:04:13 -0300

    Fix post_gen

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -4,11 +4,15 @@ import 'package:mason/mason.dart';
 Future<void> run(HookContext context) async {
   final logger = context.logger;
 
-  final platforms = (context.vars['platforms'] as String).split(',');
+  final platforms = (context.vars['platforms'] as List).cast<String>();
 
   logger.info('⚙️  Adding Flutter platforms: ${platforms.join(", ")}');
 
-  final result = await Process.run('flutter', ['create', '--platforms', platforms.join(','), '.'], runInShell: true);
+  final result = await Process.run(
+    'flutter',
+    ['create', '--platforms', platforms.join(','), '.'],
+    runInShell: true,
+  );
 
   stdout.write(result.stdout);
   stderr.write(result.stderr);
